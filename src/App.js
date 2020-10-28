856a32cd261a61599f842aaa69be0dfd3bc0978a
S662 <or.sanchez100@gmail.com>
2020-10-28 13:17:09 -0400

    add signup request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+  const requestSignup = e => {
+    e.preventDefault();
+    console.log('input data:', email, password, passwordConfirmation)
+    axios.post('http://localhost:3001/users/create', { user: {
+      'email': email,
+      'password': password,
+      'password_confirmation': passwordConfirmation
+    }}).then(response => {
+        console.log('Returned data:', response)
+    });
+  };
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={requestSignup}>
+        <label>
+          Correo electrónico:
+          <input 
+                name="email" 
+                type="text"
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          Contraseña:
+          <input 
+                name="password"
+                type="text"
+                value={password}
+                onChange={event => setPassword(event.target.value)} 
+          />
+        </label>
+        <label>
+          Confirmar contraseña:
+          <input 
+                name="passwordConfirmation"
+                type="text"
+                value={passwordConfirmation}
+                onChange={event => setPasswordConfirmation(event.target.value)} 
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
